d3367119dc56d3f7000967411815e1e7c67a7b65
Dominique Skaf <doskaf@yahoo.com>
2019-08-26 01:00:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,6 @@
 def nyc_pigeon_organizer(data)
   final = {}
   data.each do |category, category_data|
-    category_data.each do |type, 
+    category_data.each do |type, listed_names|
+      listed_names.each do 
 end
