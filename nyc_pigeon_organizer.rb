2162f66c1b61213b655c5e5f069e24f5f459b7f0
Dominique Skaf <doskaf@yahoo.com>
2019-08-26 01:30:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,16 @@
-def nyc_pigeon_organizer (data)
+def nyc_pigeon_organizer(data)
   final = {}
-
-  data.each do |first_level, all_other|
-    all_other.each do |category, array|
+  data.each do |category, category_data|
+    category_data.each do |kind, array|
       array.each do |name|
         final[name] = {:color => [], :gender => [], :lives => []}
       end 
     end 
   end 
-  x = final.keys
+  info = final.keys
   data[:color].each do |bird_color, name|
     name.each do |bird_name|
-      x.each do |item|
+      info.each do |item|
         if bird_name === item
           final[item][:color] << bird_color.to_s
         end 
@@ -20,7 +19,7 @@ def nyc_pigeon_organizer (data)
   end 
   data[:gender].each do |gender, type|
     type.each do |bird_name|
-      x.each do |item|
+      info.each do |item|
         if bird_name === item
           final[item][:gender] << gender.to_s
         end 
@@ -29,7 +28,7 @@ def nyc_pigeon_organizer (data)
   end 
   data[:lives].each do |location, name|
     name.each do |bird_name|
-      x.each do |item|
+      info.each do |item|
         if bird_name === item
           final[item][:lives] << location
         end 
